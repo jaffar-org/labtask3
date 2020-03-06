dee4c474d8c6c99c444123a6da6d38072180fe4c
jaffarabbas <gamaportal8@gmail.com>
2020-03-06 17:03:16 +0500

    Update task6.java

--- task6.java
+++ task6.java
@@ -17,12 +17,23 @@ class Main
           for (int i = 0; i< length ; i++)
           {
               char character = sen.charAt(i);
-              if(Character.isUpperCase(character) || sen.lastIndexOf('\\')=='.')
+              if(Character.isUpperCase(character) && sen.charAt(length-1)=='.')
               {
                   System.out.println("Code is Beutify");
                   break;
               }
-              else if(Character.isLowerCase(character) || sen.lastIndexOf('\\')!='.')
+              else if(Character.isLowerCase(character) && sen.charAt(length-1)=='.')
+              {
+                a =Character.toUpperCase(character);
+                System.out.println("Beutify : "+a+""+sen.substring(1));
+                break;
+              }
+              else if(Character.isUpperCase(character) && sen.charAt(length-1)!='.')
+              {
+                System.out.println("Beutify : "+sen+".");
+                break;
+              }
+              else if(Character.isLowerCase(character) && sen.charAt(length-1)!='.')
               {
                   a =Character.toUpperCase(character);
                   System.out.println("Beutify : "+a+""+sen.substring(1)+".");
